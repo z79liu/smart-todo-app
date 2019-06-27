3f2de2409221a6cfaadda5a7bea2dd3664a60531
Zeyu <haozai_03@hotmail.com>
2019-06-27 18:57:31 +0000

    connected backend with react

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+    {id: 1, username: "John"},
+    {id: 2, username: "Johny"}
+  ])
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount(){
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({users}))
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>users</h1>
+        <ul>
+          {this.state.users.map(user=> 
+          <li key={user.id}>{user.username}</li>)}
+        </ul>
       </div>
     );
   }
